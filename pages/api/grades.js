8fa2fe22b59a9b2d46052f293fd8d3270489d191
dduartee <gkduarte234@gmail.com>
2021-02-01 10:21:31 -0300

    Rota /api/specificCourseGrades integrada junto a rota /api/grades com argumentos

--- pages/api/grades.js
+++ pages/api/grades.js
@@ -12,7 +12,8 @@ async function grades(req, res) {
 
   const username = req.body.username;
   const password = req.body.password;
-  
+  const args = req.body.args;
+
   const account = await sigaa.login(username, password);
   const activeBonds = await account.getActiveBonds();
   const inactiveBonds = await account.getInactiveBonds();
@@ -20,60 +21,99 @@ async function grades(req, res) {
   var allBonds = [];
   allBonds.push(activeBonds, inactiveBonds);
 
+  function findCourse(course, args) {
+    for (const key in course) {
+      if (Object.hasOwnProperty.call(course, key)) {
+        const value = course[key];
+        for (const argKEY in args) {
+          if (Object.hasOwnProperty.call(args, argKEY)) {
+            const arg = args[argKEY];
+            if (arg == value) {
+              return course;
+            }
+          }
+        }
+      }
+    }
+    return 0;
+  }
+
+  function pushGrades(gradesGroups) {
+    var gradesJSON = [];
+    for (const gradesGroup of gradesGroups) {
+      switch (gradesGroup.type) {
+        case 'only-average':
+          gradesJSON.push({
+            name: gradesGroup.name,
+            value: gradesGroup.value
+          })
+          break;
+        case 'weighted-average':
+          var personalGrade = [];
+          for (const grade of gradesGroup.grades) {
+            personalGrade.push({
+              name: grade.name,
+              weight: grade.weight,
+              value: grade.value,
+            })
+          }
+          gradesJSON.push({
+            personalGrade: personalGrade,
+            groupGrade: gradesGroup.value
+          })
+          break;
+        case 'sum-of-grades':
+          var personalGrade = [];
+          for (const grade of gradesGroup.grades) {
+            personalGrade.push({
+              name: grade.name,
+              maxValue: grade.maxValue,
+              value: grade.value
+            })
+          }
+          gradesJSON.push({
+            personalGrade: personalGrade,
+            groupGrade: gradesGroup.value
+          })
+          break;
+      }
+    }
+    return gradesJSON;
+  }
+
   for (const bonds of allBonds) {
     for (let i = 0; i < bonds.length; i++) {
       coursesJSON = [];
       const bond = bonds[i];
       var courses = await bond.getCourses();
       for (const course of courses) {
-        gradesJSON = [];
-        const gradesGroups = await course.getGrades();
-        for (const gradesGroup of gradesGroups) {
-          switch (gradesGroup.type) {
-            case 'only-average':
-              gradesJSON.push({
-                name: gradesGroup.name,
-                value: gradesGroup.value
-              })
-              break;
-            case 'weighted-average':
-              var personalGrade = [];
-              for (const grade of gradesGroup.grades) {
-                personalGrade.push({
-                  name: grade.name,
-                  weight: grade.weight,
-                  value: grade.value,
-                })
-              }
-              gradesJSON.push({
-                personalGrade: personalGrade,
-                groupGrade: gradesGroup.value
-              })
-              break;
-            case 'sum-of-grades':
-              var personalGrade = [];
-              for (const grade of gradesGroup.grades) {
-                personalGrade.push({
-                  name: grade.name,
-                  maxValue: grade.maxValue,
-                  value: grade.value
-                })
-              }
-              gradesJSON.push({
-                personalGrade: personalGrade,
-                groupGrade: gradesGroup.value
-              })
-              break;
+        if (args) { //se tiver argumentos
+          const valid = findCourse(course, args) //encontre o curso
+          if (valid) { //se existir o curso
+            const gradesGroups = await course.getGrades();
+            const gradesJSON = pushGrades(gradesGroups);
+            coursesJSON.push({
+              id: course.id,
+              title: course.title,
+              code: course.code,
+              period: course.period,
+              scheudule: course.scheudule,
+              grades: gradesJSON
+            })
           }
+        } else { //se nao
+          const gradesGroups = await course.getGrades();
+          const gradesJSON = pushGrades(gradesGroups);
+          coursesJSON.push({
+            id: course.id,
+            title: course.title,
+            code: course.code,
+            period: course.period,
+            scheudule: course.scheudule,
+            grades: gradesJSON
+          })
         }
-        coursesJSON.push({
-          id: course.id,
-          title: course.title,
-          code: course.code,
-          period: course.period,
-          scheudule: course.scheudule,
-          grades: gradesJSON
-        })
+        console.log(coursesJSON);
       }
       bondsJSON.push({
         program: bond.program,
@@ -88,7 +128,7 @@ async function grades(req, res) {
   })
 
   res.json(resultJSON);
-  
+
   await account.logoff();
 }
 
