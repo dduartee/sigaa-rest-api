fecd1a684492a6d855a47c372246e3225ec13898
dduartee <gabrielkduarte234@gmail.com>
2021-02-20 22:01:08 -0300

    body-parser removido

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
 const api = require('./src/api/routes');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.use(cors());
 
