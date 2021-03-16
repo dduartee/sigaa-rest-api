c6d76efeb50fe5a02a94603f5bc13eb40d24d7af
dduartee <gabrielkduarte234@gmail.com>
2021-03-16 10:30:34 -0300

    integrando a rota demo com demonstração

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 const app = express();
 
 import api from './api/routes';
 import version from './version';
 import headers from './headers';
 import body from './body';
-
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(cors());
-
 app.use('/api', api);
 app.get('/version', version);
 app.all('/headers', headers);
 app.all('/body', body);
+app.get('/demo', (req, res) => {
+    res.sendFile(path.join(__dirname+"/public/index.html"))
+});
 
 app.get('/', (req, res) => {
     res.send("Sigaa-rest-api")
